8c5fa010f5c88af23edb3e9972225a17e679457a
benjaminstern <benjamin.matteo.stern@gmail.com>
2016-09-22 13:36:41 +0200

    errors behoben

--- app/src/main/java/com/nfchecklist/app/NewTagActivity.java
+++ app/src/main/java/com/nfchecklist/app/NewTagActivity.java
@@ -1,16 +1,13 @@
 package com.nfchecklist.app;
 
-import android.os.Bundle;
-import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 
-/**
- * Created by admin on 22.09.2016.
- */
 public class NewTagActivity extends AppCompatActivity {
+
     @Override
-    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
-        super.onCreate(savedInstanceState, persistentState);
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_tag);
     }
 }
