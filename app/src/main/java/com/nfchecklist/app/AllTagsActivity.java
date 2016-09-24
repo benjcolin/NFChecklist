eb777c9494199f8c33a1af97b20f3dc41ae33ae5
benjaminstern <benjamin.matteo.stern@gmail.com>
2016-09-24 20:59:11 +0200

    Fragments eingebaut

--- app/src/main/java/com/nfchecklist/app/AllTagsActivity.java
+++ app/src/main/java/com/nfchecklist/app/AllTagsActivity.java
@@ -3,9 +3,12 @@ package com.nfchecklist.app;
 import android.app.Activity;
 import android.content.Intent;
 import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.widget.SimpleCursorAdapter;
 import android.view.View;
 import android.support.design.widget.NavigationView;
@@ -18,88 +21,162 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.AdapterView;
 import android.widget.ListView;
-import android.widget.TextView;
 import android.widget.Toast;
 
-public class AllTagsActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
-    private ListView listView;
-    private DBHelper dbHelper;
-    private SimpleCursorAdapter cursorAdapter;
+public class AllTagsActivity extends AppCompatActivity implements AllTagsFragment.OnFragmentInteractionListener, ChecklistFragment.OnFragmentInteractionListener {
+
+    private static final String MENU_ALLTAGS = "alltags";
+    private static final String MENU_CHECKLIST = "checklist";
+    public static String CURRENT_MENU = MENU_ALLTAGS;
+    public static int navItemIndex = 0;
+
+    private NavigationView navigationView;
+    private DrawerLayout drawer;
+    private String[] activityTitles;
+    private Handler mHandler;
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_all_tags);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.setDrawerListener(toggle);
-        toggle.syncState();
+        drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
 
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
+        navigationView = (NavigationView) findViewById(R.id.nav_view);
+        activityTitles = getResources().getStringArray(R.array.nav_item_acitivity_titles);
+        mHandler = new Handler();
 
-        dbHelper = new DBHelper(this);
+        setUpNavigationView();
 
-        final Cursor cursor = dbHelper.getAllTags();
-        String [] columns = new String[]{
-                DBHelper.TAG_COLUMN_NAME,
-                DBHelper.TAG_COLUMN_ID
-        };
-        int [] widgets = new int[]{
-                R.id.tagName,
-                R.id.tagId
-        };
-
-        cursorAdapter = new SimpleCursorAdapter(this, R.layout.tag,
-                cursor, columns, widgets, 0);
-        listView = (ListView) findViewById(R.id.listView);
-        listView.setAdapter(cursorAdapter);
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
-                Cursor itemCursor = (Cursor) AllTagsActivity.this.listView.getItemAtPosition(position);
-                dbHelper.deleteTag(itemCursor.getInt(itemCursor.getColumnIndex(DBHelper.TAG_COLUMN_ID)));
-                final Cursor cursor = dbHelper.getAllTags();
-                cursorAdapter.changeCursor(cursor);
-            }
-        });
+        if (savedInstanceState == null) {
+            navItemIndex = 0;
+            CURRENT_MENU = MENU_ALLTAGS;
+            loadHomeFragment();
+        }
     }
 
-    public void newTag(View view){
+    private void loadHomeFragment() {
+        selectNavMenu();
+        setToolbarTitle();
+
+        if (getSupportFragmentManager().findFragmentByTag(CURRENT_MENU) != null) {
+            drawer.closeDrawers();
+            return;
+        }
+
+        Runnable mPendingRunnable = new Runnable() {
+            @Override
+            public void run() {
+                Fragment fragment = getHomeFragment();
+                FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                fragmentTransaction.replace(R.id.frame, fragment, CURRENT_MENU);
+                fragmentTransaction.commitAllowingStateLoss();
+            }
+        };
+
+        if (mPendingRunnable != null) {
+            mHandler.post(mPendingRunnable);
+        }
+        drawer.closeDrawers();
+        invalidateOptionsMenu();
+    }
+
+    private void setUpNavigationView() {
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem menuItem) {
+                switch (menuItem.getItemId()) {
+                    case R.id.alltags:
+                        navItemIndex = 0;
+                        CURRENT_MENU = MENU_ALLTAGS;
+                        break;
+                    case R.id.checklist:
+                        navItemIndex = 1;
+                        CURRENT_MENU = MENU_CHECKLIST;
+                        break;
+                    default:
+                        navItemIndex = 0;
+                }
+
+                if (menuItem.isChecked()) {
+                    menuItem.setChecked(false);
+                } else {
+                    menuItem.setChecked(true);
+                }
+                menuItem.setChecked(true);
+                loadHomeFragment();
+                return true;
+            }
+        });
+
+        ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+    }
+
+    private Fragment getHomeFragment() {
+        switch (navItemIndex) {
+            case 0:
+                AllTagsFragment allTagsFragment = new AllTagsFragment();
+                return allTagsFragment;
+            case 1:
+                ChecklistFragment checklistFragment = new ChecklistFragment();
+                return checklistFragment;
+            default:
+                return new AllTagsFragment();
+        }
+    }
+
+    private void setToolbarTitle() {
+        getSupportActionBar().setTitle(activityTitles[navItemIndex]);
+    }
+
+    private void selectNavMenu() {
+        navigationView.getMenu().getItem(navItemIndex).setChecked(true);
+    }
+
+    public void newTag(View view) {
         Intent intent = new Intent(this, NewTagActivity.class);
         startActivityForResult(intent, NewTagActivity.REQUEST_NEW_TAG);
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == NewTagActivity.REQUEST_NEW_TAG && resultCode == Activity.RESULT_OK){
-            dbHelper.insertTag(data.getStringExtra(NewTagActivity.TAG_NAME));
-            final Cursor cursor = dbHelper.getAllTags();
-            cursorAdapter.changeCursor(cursor);
+        if (requestCode == NewTagActivity.REQUEST_NEW_TAG && resultCode == Activity.RESULT_OK) {
+            //dbHelper.insertTag(data.getStringExtra(NewTagActivity.TAG_NAME));
+            //final Cursor cursor = dbHelper.getAllTags();
+            //cursorAdapter.changeCursor(cursor);
         }
     }
 
     @Override
     public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         if (drawer.isDrawerOpen(GravityCompat.START)) {
-            drawer.closeDrawer(GravityCompat.START);
-        } else {
-            super.onBackPressed();
+            drawer.closeDrawers();
+            return;
         }
+
+        if (navItemIndex != 0) {
+            navItemIndex = 0;
+            CURRENT_MENU = MENU_ALLTAGS;
+            loadHomeFragment();
+            return;
+        }
+
+        super.onBackPressed();
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.all_tags, menu);
+        if (navItemIndex == 0) {
+            getMenuInflater().inflate(R.menu.all_tags, menu);
+        }
         return true;
     }
 
@@ -118,28 +195,8 @@ public class AllTagsActivity extends AppCompatActivity
         return super.onOptionsItemSelected(item);
     }
 
-    @SuppressWarnings("StatementWithEmptyBody")
     @Override
-    public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
-        int id = item.getItemId();
+    public void onFragmentInteraction(Uri uri) {
 
-//        if (id == R.id.nav_camera) {
-//            // Handle the camera action
-//        } else if (id == R.id.nav_gallery) {
-//
-//        } else if (id == R.id.nav_slideshow) {
-//
-//        } else if (id == R.id.nav_manage) {
-//
-//        } else if (id == R.id.nav_share) {
-//
-//        } else if (id == R.id.nav_send) {
-//
-//        }
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        drawer.closeDrawer(GravityCompat.START);
-        return true;
     }
 }
---
+++ app/src/main/java/com/nfchecklist/app/AllTagsFragment.java
@@ -0,0 +1,140 @@
+package com.nfchecklist.app;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.widget.CursorAdapter;
+import android.support.v4.widget.SimpleCursorAdapter;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link AllTagsFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link AllTagsFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class AllTagsFragment extends Fragment {
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    private OnFragmentInteractionListener mListener;
+
+    public AllTagsFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment AllTagsFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static AllTagsFragment newInstance(String param1, String param2) {
+        AllTagsFragment fragment = new AllTagsFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+
+//        DBHelper dbHelper = new DBHelper(this);
+//
+//        final Cursor cursor = dbHelper.getAllTags();
+//        String[] columns = new String[]{
+//                DBHelper.TAG_COLUMN_NAME,
+//                DBHelper.TAG_COLUMN_ID
+//        };
+//        int[] widgets = new int[]{
+//                R.id.tagName,
+//                R.id.tagId
+//        };
+
+//        SimpleCursorAdapter cursorAdapter = new SimpleCursorAdapter(this, R.layout.tag,
+//                cursor, columns, widgets, 0);
+//        ListView listView = (ListView) findViewById(R.id.listView);
+//        listView.setAdapter(cursorAdapter);
+//        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+//            @Override
+//            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int position, long id) {
+//                Cursor itemCursor = (Cursor) AllTagsActivity.this.listView.getItemAtPosition(position);
+//                dbHelper.deleteTag(itemCursor.getInt(itemCursor.getColumnIndex(DBHelper.TAG_COLUMN_ID)));
+//                final Cursor cursor = dbHelper.getAllTags();
+//                cursorAdapter.changeCursor(cursor);
+//                return true;
+//            }
+//        });
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_all_tags, container, false);
+    }
+
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(Uri uri);
+    }
+}
